8ecf10fa7ea7e9cc837b51c78b593bc105102896
Srishan Shakya <csunshakya@gmail.com>
2022-08-15 16:32:34 +0545

    new Flutter 3.0 build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,45 +3,45 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "3b655c0b98e58b4c71f3609a102a274d",
+  "assets/AssetManifest.json": "329f7271aa380a4f9dab279c516ccd0e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "c269da1bc7f39d4b2e159a006f4a81fc",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/NOTICES": "34c51ed7c45a1662eb30d2f840d6d983",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/packages/textaba_avataaars/assets/accessory.svg": "7035491085ebcfde73d45f4e809d8ce0",
-"assets/packages/textaba_avataaars/assets/cloth.svg": "3ddf2fac1a2ff1d658ce905b10651fbc",
-"assets/packages/textaba_avataaars/assets/cloth_color.svg": "118d98bf391be899b8713e26e2af8d9e",
-"assets/packages/textaba_avataaars/assets/eye.svg": "8a4fc4d15a462a2818ad1c8356520f64",
-"assets/packages/textaba_avataaars/assets/eyebrow.svg": "6c823bd7909d31fa994b4e1116922a2b",
-"assets/packages/textaba_avataaars/assets/facial_hair.svg": "a333b7aacce6030396f76e54e6d78951",
-"assets/packages/textaba_avataaars/assets/facial_hair_color.svg": "9a96155cf05e32e4202ccb5e84dc00bf",
-"assets/packages/textaba_avataaars/assets/hair.svg": "0726cada45658a2278360d5b7d6e2b02",
-"assets/packages/textaba_avataaars/assets/hair_color.svg": "4cecba0835dad27d06899aab45c03885",
-"assets/packages/textaba_avataaars/assets/mouth.svg": "019acb77500d092d3347818a4cc583e5",
-"assets/packages/textaba_avataaars/assets/skin.svg": "1c6f80e6be0e8eb227b90cae0895a396",
-"canvaskit/canvaskit.js": "62b9906717d7215a6ff4cc24efbd1b5c",
-"canvaskit/canvaskit.wasm": "b179ba02b7a9f61ebc108f82c5a1ecdb",
-"canvaskit/profiling/canvaskit.js": "3783918f48ef691e230156c251169480",
-"canvaskit/profiling/canvaskit.wasm": "6d1b0fc1ec88c3110db88caa3393c580",
+"assets/packages/flutter_avataaars/assets/accessory.svg": "7035491085ebcfde73d45f4e809d8ce0",
+"assets/packages/flutter_avataaars/assets/cloth.svg": "3ddf2fac1a2ff1d658ce905b10651fbc",
+"assets/packages/flutter_avataaars/assets/cloth_color.svg": "118d98bf391be899b8713e26e2af8d9e",
+"assets/packages/flutter_avataaars/assets/eye.svg": "8a4fc4d15a462a2818ad1c8356520f64",
+"assets/packages/flutter_avataaars/assets/eyebrow.svg": "6c823bd7909d31fa994b4e1116922a2b",
+"assets/packages/flutter_avataaars/assets/facial_hair.svg": "a333b7aacce6030396f76e54e6d78951",
+"assets/packages/flutter_avataaars/assets/facial_hair_color.svg": "9a96155cf05e32e4202ccb5e84dc00bf",
+"assets/packages/flutter_avataaars/assets/hair.svg": "0726cada45658a2278360d5b7d6e2b02",
+"assets/packages/flutter_avataaars/assets/hair_color.svg": "4cecba0835dad27d06899aab45c03885",
+"assets/packages/flutter_avataaars/assets/mouth.svg": "019acb77500d092d3347818a4cc583e5",
+"assets/packages/flutter_avataaars/assets/skin.svg": "1c6f80e6be0e8eb227b90cae0895a396",
+"assets/shaders/ink_sparkle.frag": "92514212ce53a9136d5fcd4f9279b044",
+"canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
+"canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
+"canvaskit/profiling/canvaskit.js": "38164e5a72bdad0faa4ce740c9b8e564",
+"canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "66d804fde39310963db41937d1e165e4",
-"/": "66d804fde39310963db41937d1e165e4",
-"main.dart.js": "cddfd80542aa522538b2ee28da4c3dff",
-"manifest.json": "9a0de1ff26661cd281cb9deb3db33b02",
+"index.html": "06e2e344eb0ec332ffcc0fe1a4ad55ee",
+"/": "06e2e344eb0ec332ffcc0fe1a4ad55ee",
+"main.dart.js": "abb4ef9a349de89598582ee98eb9ac4c",
+"manifest.json": "d3b1a47ff5a1524af31aba54130a7f48",
 "version.json": "ff966ab969ba381b900e61629bfb9789"
 };
 
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
-"main.dart.js",
+  "main.dart.js",
 "index.html",
-"assets/NOTICES",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
 // During install, the TEMP cache is populated with the application shell files.
@@ -140,9 +140,11 @@ self.addEventListener("fetch", (event) => {
     .then((cache) =>  {
       return cache.match(event.request).then((response) => {
         // Either respond with the cached resource, or perform a fetch and
-        // lazily populate the cache.
+        // lazily populate the cache only if the resource was successfully fetched.
         return response || fetch(event.request).then((response) => {
-          cache.put(event.request, response.clone());
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
           return response;
         });
       })
